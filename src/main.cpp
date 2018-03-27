fb8e79b854e2326ca240efced4ecafa2338f7589
jptomanibarra <jptomanibarra@email.arizona.edu>
2018-03-27 16:51:39 -0700

    uploading current code, which is giving serial monitor trouble

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,27 @@
 #include "myspi.h"
 #include "tripwire.h"
 
+// int adcVal;
+
 int main(void) {
-  initLED();
+  // Serial.begin(115200);
+  // Serial.print("test\n");
+  // initLED();
 
-  turnOnLED();
+  Serial.begin(9600);
 
-  // while(1) {
-  //   turnOnLED();
-  //   delayMicroseconds(50000);
-  //   turnOffLED();
-  //   delayMicroseconds(50000);
-  // }
+  // turnOnLED();
+
+  while(1) {
+    // adcVal = analogRead(A0);
+    Serial.println("test\n");
+  }
+  // // while(1) {
+  // //   turnOnLED();
+  // //   delayMicroseconds(50000);
+  // //   turnOffLED();
+  // //   delayMicroseconds(50000);
+  // // }
 
   return(0);
 }
