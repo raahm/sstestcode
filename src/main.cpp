d7251fe6595ab98aa0b57182b497b6fa6c3e42d9
jptomanibarra <jptomanibarra@email.arizona.edu>
2018-04-17 18:30:17 -0700

    adding changes to CA

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,18 @@
 #include "tripwire.h"
 
 int adcTension;
+int beadCount;
+int beadDelta;
+int beadStart;
+double tension;
+double tensionPrev;
+double tensionDelta;
+float circumference;
 
 int main(void) {
   Serial.begin(9600); // serial monitor with 9600 baud rate
+  beadCount = 10;	// this value will be changed to whatever the actual start is
+
 
   while(1) {
     adcTension = analogRead(A0);
